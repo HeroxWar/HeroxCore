38ebe916b1cc774061f88e6ffb4cc6081b7581b9
Elio Testa <eliotesta1998@libero.it>
2026-04-06 00:17:38 +0200

    Version 1.3.16

--- src/test/java/com/HeroxWar/HeroxCore/Utils/VersionTest.java
+++ src/test/java/com/HeroxWar/HeroxCore/Utils/VersionTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockbukkit.mockbukkit.MockBukkit;
 import org.mockbukkit.mockbukkit.ServerMock;
 
+import java.util.Arrays;
+
 public class VersionTest {
 
     private ServerMock serverMock;
@@ -19,6 +21,9 @@ public class VersionTest {
         // Inizialization server and plugin
         serverMock = MockBukkit.mock();
         version = new Version();
+        System.out.println(version.getFormattedServerVersion());
+        System.out.println(Arrays.toString(version.getSplitFormattedServerVersion()));
+        System.out.println(version.getServerVersion());
     }
 
     @AfterEach
@@ -28,10 +33,19 @@ public class VersionTest {
     }
 
     @Test
+    public void testLegacyLibraries() {
+        version.setServerVersion("MockBukkit (MC: 1.21.11)");
+        Assertions.assertFalse(version.isInRange(8, 16));
+    }
+
+    @Test
     public void testVersion() {
-        Assertions.assertEquals("MockBukkit (MC: 1.21.4)", version.getServerVersion());
         version.setServerVersion("MockBukkit (MC: 1.21)");
         Assertions.assertEquals("MockBukkit (MC: 1.21)", version.getServerVersion());
+        version.setFormattedServerVersion("36.12.1");
+        Assertions.assertEquals("36.12.1", version.getFormattedServerVersion());
+        version.setSplitFormattedServerVersion(new String[] {"36", "12", "1"});
+        Assertions.assertArrayEquals(new String[] {"36", "12", "1"}, version.getSplitFormattedServerVersion());
     }
 
     @Test
@@ -51,11 +65,12 @@ public class VersionTest {
         version.setServerVersion("MockBukkit (MC: 26.1)");
         Assertions.assertTrue(version.isEquals(26));
         Assertions.assertFalse(version.isEquals(25));
+        Assertions.assertFalse(version.isEquals(27));
     }
 
     @Test
     public void testMatchRangeVersion26() {
-        version.setServerVersion("MockBukkit (MC: 26.1)");
+        version.setServerVersion("MockBukkit (MC: 26.1.1)");
         Assertions.assertTrue(version.isInRange(21, 26));
         Assertions.assertFalse(version.isInRange(21, 25));
     }
